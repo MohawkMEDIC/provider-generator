8f8b7b2bc113a3687e911e7ae2ba6d51d4668cc5
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-05-15 18:13:24 -0400

    added a service reference to the provider generator service

---
+++ ProviderGenerator.Web/Controllers/GenerationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProviderGenerator.Web.Controllers
+{
+    public class GenerationController : Controller
+    {
+        // GET: Generation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProviderGenerator.Web/Models/DataModels/GenerationDataModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProviderGenerator.Web.Models.DataModels
+{
+	public class GenerationDataModel
+	{
+		public GenerationDataModel()
+		{
+		}
+
+		[Display(Name = "Client Registry")]
+		public bool ClientRegistry { get; set; }
+
+		public bool HNS { get; set; }
+
+		public bool OLIS { get; set; }
+
+		[Display(Name = "Provider Registry")]
+		public bool ProviderRegistry { get; set; }
+
+		[Display(Name = "Age Distribution")]
+		public double[] AgeDistribution { get; set; }
+
+		[Display(Name = "Gender Distribution")]
+		public double[] GenderDistribution { get; set; }
+
+		[Display(Name = "Number of Records")]
+		public int NumberOfRecords { get; set; }
+
+		[Display(Name = "% of Providers in Organizations")]
+		public double ProviderOrganizationPercentage { get; set; }
+	}
+}
